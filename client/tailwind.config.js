9862def23b30a45e1db4ef73c92995812f142088
CristianJoseMoreno <cjmorenoberlanga@gmail.com>
2025-05-31 15:31:19 +0200

    Add:Primera version de UI, empezamos a separar footer y dar estilos

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,12 +8,12 @@ module.exports = {
           foreground: "hsl(211 100% 99%)",
         },
         "bg-default-section": "#F3F4F6", // Color para la mayoría de las secciones (Hero, ¿Por qué elegir?, Footer)
-        "bg-about-us-mint": "#E0F8F0", // Color para "Sobre Nosotros"
+        "bg-about-us-mint": "#B9FDE7", // Color para "Sobre Nosotros"
       },
       boxShadow: {
         md: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
         lg: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
-        xl: "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
+        xl: "0 20px 25px -5px rgba(122, 167, 111, 0.59), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
       },
       transitionProperty: {
         shadow: "box-shadow",
